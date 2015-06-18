c782275d614d1dc8ed48a6bb5b7201b9d65b2a93
Mike Ball <mikedball@gmail.com>
2015-06-18 17:03:58 -0400

    add Should model tests

---
+++ spec/models/should_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe Should, type: :model do
+  before :each do
+    @should = Should.new(user: 'mike', context: 'some should usage')
+  end
+
+  describe '#user' do
+    it 'returns the correct "user" value from the model' do
+      expect(@should.user).to eq 'mike'
+    end
+  end
+
+  describe '#context' do
+    it 'returns the correct "context" value from the model' do
+      expect(@should.context).to eq 'some should usage'
+    end
+  end
+end
