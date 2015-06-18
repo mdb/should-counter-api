413791d97bf5d18ad35e349ea3c26048ecfe1b1b
Mike Ball <mikedball@gmail.com>
2015-06-18 16:20:41 -0400

    rails g resource should --force

--- db/migrate/20150618201610_create_shoulds.rb
+++ db/migrate/20150618201610_create_shoulds.rb
@@ -1,8 +1,6 @@
 class CreateShoulds < ActiveRecord::Migration
   def change
     create_table :shoulds do |t|
-      t.string :context
-      t.string :user
 
       t.timestamps null: false
     end
