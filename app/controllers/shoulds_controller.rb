839abf5b9545db63e6c14b83592a5b66eaf54b5a
Mike Ball <mikedball@gmail.com>
2015-06-18 22:39:58 -0400

    add ShouldsController

--- app/controllers/shoulds_controller.rb
+++ app/controllers/shoulds_controller.rb
@@ -1,2 +1,36 @@
 class ShouldsController < ApplicationController
+  before_action :set_should, only: [:show]
+
+  # GET /shoulds
+  def index
+    @shoulds = Should.all
+
+    render json: @shoulds
+  end
+
+  # GET /shoulds/1
+  def show
+    render json: @should
+  end
+
+  # POST /shoulds
+  def create
+    @should = Should.new(should_params)
+
+    if @should.save
+      render json: @should, status: :created, location: @should
+    else
+      render json: @should.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_should
+    @should = Should.find(params[:id])
+  end
+
+  def should_params
+    params.require(:should).permit(:user, :context)
+  end
 end
---
+++ spec/controllers/should_controller_spec.rb
@@ -0,0 +1,51 @@
+require 'rails_helper'
+
+RSpec.describe ShouldsController, type: :controller do
+  before do
+    Should.new(user: 'user', context: 'context').save
+  end
+
+  describe '#index' do
+    before do
+      get :index
+    end
+
+    it 'returns success' do
+      expect(response).to be_success
+    end
+
+    it 'returns a JSON array of all the Shoulds' do
+      expect(JSON.parse(response.body)[0]['user']).to eq 'user'
+      expect(JSON.parse(response.body)[0]['context']).to eq 'context'
+    end
+  end
+
+  describe '#show' do
+    before do
+      get :show, id: 1
+    end
+
+    it 'returns success' do
+      expect(response).to be_success
+    end
+
+    it 'shows the correct Should instance' do
+      expect(JSON.parse(response.body)['user']).to eq 'user'
+      expect(JSON.parse(response.body)['context']).to eq 'context'
+    end
+  end
+
+  describe '#create' do
+    before do
+      post :create, should: { user: 'a user', context: 'some should context' }
+    end
+
+    it 'returns success' do
+      expect(response).to be_success
+    end
+
+    it 'saves the should data it is is sent' do
+      expect(Should.find_by_user('a user').context).to eq 'some should context'
+    end
+  end
+end
